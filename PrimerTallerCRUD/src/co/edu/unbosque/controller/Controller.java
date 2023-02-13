6ae9493be9fd064c41b9d2fc7cf937b5c397ab27
HP <HP@DESKTOP-P1MV2V2>
2023-02-13 00:25:38 -0500

    Update final validations

--- PrimerTallerCRUD/src/co/edu/unbosque/controller/Controller.java
+++ PrimerTallerCRUD/src/co/edu/unbosque/controller/Controller.java
@@ -21,42 +21,89 @@ public class Controller {
 	}
 	
 	private void menu() {
+		
 		String option = view.readInfo("\n \nIngresa la opción: "
 									 + "\n1. Agregar Candidato "
 									 + "\n2. Eliminar Candidato "
-									 + "\n3. Ver arreglo "
+									 + "\n3. Ver Candidatos "
 									 + "\n4. Modificar Candidato "
 									 + "\n5. Buscar Candidato "
 									 + "\n0. Salir");
 		
 		switch (option) {
+		
 		case "1":
 			//String nombre, String apellido, String cedula, int edad, String cargo
 			String newName = view.readInfo("\nIngrese nombre del candidato: ");
+			if(newName.matches(".*[0-9].*")) {
+				view.showMessage("¡ERROR! Ingrese un nombre valido");
+				menu();
+			}
 			String newLastName = view.readInfo("\nIngrese apellido del cantidato: ");
+			if(newLastName.matches(".*[0-9].*")) {
+				view.showMessage("¡ERROR! Ingrese un apellido valido");
+				menu();
+			}
 			String newId = view.readInfo("\nIngrese cedula del candidato: ");
+			for (int i = 0; i < newId.length(); i++) {
+	            if (!Character.isDigit(newId.charAt(i))) {
+	            	view.showMessage("¡ERROR! Ingrese una cedula valida");
+					menu();
+	            }
+			}
 			String newAge = view.readInfo("\nIngrese edad del candidato: ");
+			for (int i = 0; i < newId.length(); i++) {
+	            if (!Character.isDigit(newId.charAt(i))) {
+	            	view.showMessage("¡ERROR! Ingrese una edad valida");
+					menu();
+	            }
+			}
 			String newWorkstation = view.readInfo("\nIngrese cargo del candidato: ");
 			
 			emp.create(newName, newLastName, newId, newAge, newWorkstation);
 			menu();
 		break;
+		
 		case "2":
 			String idToDelete = view.readInfo("\nIngrese el numero de cedula del candidato que desea eliminar: ");
 			emp.delete(idToDelete);
 			menu();
 		break;
+		
 		case "3":
 			emp.showList();
 			menu();
 		break;
+		
 		case "4":
 			String candidateId = view.readInfo("\nIngrese la cedula del candidato a buscar: ");
 			if(emp.read(candidateId)!=null) {
 				String newCandidateId = view.readInfo("Ingrese el nuevo numero de cedula: ");
+				for (int i = 0; i < newCandidateId.length(); i++) {
+		            if (!Character.isDigit(newCandidateId.charAt(i))) {
+		            	view.showMessage("¡ERROR! Ingrese una cedula valida");
+						menu();
+		            }
+				}
+				
 				String newCandidateName = view.readInfo("Ingrese nuevo nombre: ");
+				if(newCandidateName.matches(".*[0-9].*")) {
+					view.showMessage("¡ERROR! Ingrese un nombre valido");
+					menu();
+				}
+				
 				String newCandidateLastName = view.readInfo("Ingrese nuevo apellido: ");
+				if(newCandidateLastName.matches(".*[0-9].*")) {
+					view.showMessage("¡ERROR! Ingrese un apellido valido");
+					menu();
+				}
 				String newCandidateAge = view.readInfo("Ingrese nueva edad: ");
+				for (int i = 0; i < newCandidateId.length(); i++) {
+		            if (!Character.isDigit(newCandidateId.charAt(i))) {
+		            	view.showMessage("¡ERROR! Ingrese una edad valida");
+						menu();
+		            }
+				}
 				String newCandidateWorkstation = view.readInfo("Ingrese nuevo cargo: ");
 				
 				emp.update(candidateId, newCandidateName, newCandidateLastName, newCandidateId, newCandidateAge, newCandidateWorkstation);
